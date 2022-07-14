5a52cf9ef7dcb41d7e3724d1dcf2674293748a15
Jason Ellison <jason.w.ellison@gmail.com>
2022-07-14 15:50:31 -0400

    Added ability to search businesses by business name

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -1,5 +1,44 @@
 import { getBusinesses } from "./database.js"
 
+document
+    .querySelector("#content")
+        .addEventListener(
+            "keypress",
+            (keyPressEvent) => {
+                const companySearchResultArticle = document.querySelector(".foundBusinesses")
+                const businesses = getBusinesses()
+                let foundBusinessHTML = ''
+
+                if (keyPressEvent.charCode === 13) {
+                    /*
+                        When the user presses 'Enter', find the matching business.
+
+
+                        You can use the `.includes()` string method to
+                        see if a smaller string is part of a larger string.
+
+                        Example:
+                            if (business.companyName.includes(keyPressEvent.target.value)) {
+
+                            }
+                    */
+
+                    const foundBusiness = businesses.find(
+                        business => {
+                            if (!business?.companyName?.toLowerCase().includes(keyPressEvent.target.value.toLowerCase())) return
+                            return business
+                        }
+                    )
+
+                    foundBusinessHTML = `<h2>${foundBusiness?.companyName}</h2>
+                    <p>${foundBusiness?.addressFullStreet}</p>
+                    <p>${foundBusiness?.addressCity}, ${foundBusiness?.addressStateCode} ${foundBusiness?.addressZipCode}</p>`
+
+                    companySearchResultArticle.innerHTML = `${foundBusinessHTML}`
+                }
+        });
+
+
 /*
     BusinessList Component Function
 
